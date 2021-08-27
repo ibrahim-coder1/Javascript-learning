6447a19328acb1cc6f38d72e962de2d14ac9474a
ibrahim-coder1 <ibrahimcoder1@gmail.com>
2021-08-27 20:54:54 +0600

    varibale

---
+++ milestone-1/variable.js
@@ -0,0 +1,25 @@
+// vary + able = variable
+// কোন কিছুর মান যেটা পরিবর্তন যোগ্য ।
+// কোন কিছুর মান প্রকাশ করে ,
+// variable কে var দিয়ে প্রকাশ করে javascript এ ।
+// কোন কিছুর মান রাখার জন্য বা প্রকাশ করার জন্য বা নির্দিস্ট করার জন্য var ব্যবহার করা হয় ।
+// var প্রকাশ করার জন্য ৫ টা জিনিস লিখতে হয় ।
+// vary মানে বিভিন্ন ।
+
+// Numeric variable
+var age = 35;
+var applePrice = 25;
+var tempaterute = 37;
+
+
+// String variable. string মানে সুতা বা রশি 
+var name = "abul kashem";
+var address = "mirpur";
+var scholl ""
+
+
+//boolean variable. এটা দ্বারা সত্য মিথ্যা প্রকাশ করা হয় ।
+var pass = true;
+var fail = false;
+
+//true false string এর মত হলে এগুলো স্পেশাল কিওয়ার্ড । এগুলো javascript থেকে নির্ধারন করা ।
